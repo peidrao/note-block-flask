7d90dfa4a7346b803ee7eead862b1a0424b61281
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-03 22:38:20 -0300

    update app

--- src/app.py
+++ src/app.py
@@ -3,13 +3,14 @@ from flask_restful import Resource, Api
 
 from dotenv import dotenv_values
 from database.connect import create_db_and_tables
+from utils.constants import HTTP_200_ACCEPTED
+from services.profile import ProfileListView
 
 config = dotenv_values(".env")
 
 
 app = Flask(__name__)
 api = Api(app)
-
 
 
 @app.before_first_request
@@ -21,10 +22,11 @@ def create_db():
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'world'}, HTTP_200_ACCEPTED
 
 
 api.add_resource(HelloWorld, '/')
+api.add_resource(ProfileListView, '/profile')
 
 if __name__ == '__main__':
     app.run(host=config.get('HOST'), port=8080, debug=config.get('DEBUG'))
