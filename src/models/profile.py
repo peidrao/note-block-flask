a3c45fbac3948e6b31ee33f1597a1ae81ad484e0
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-04 23:25:28 -0300

    update model and schema profile

--- src/models/profile.py
+++ src/models/profile.py
@@ -6,5 +6,8 @@ from sqlmodel import Field, SQLModel
 class Profile(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
+    username: str
+    email: str
+    password: str
     is_active: bool = Field(default=True)
     created_at: datetime = Field(default=datetime.utcnow(), nullable=False)
--- src/schemas/profile.py
+++ src/schemas/profile.py
@@ -4,4 +4,12 @@ from marshmallow import Schema, fields
 class ProfileSchema(Schema):
     id = fields.Int()
     name = fields.Str()
+    username = fields.Str()
+    email = fields.Str()
+    password = fields.Str()
     created_at = fields.DateTime()
+
+
+class ProfileLoginSchema(Schema):
+    username = fields.Str()
+    password = fields.Str()
