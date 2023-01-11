941e1d88785438b982f84b7bed29266f8846cbe0
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-10 23:17:42 -0300

    config celery

--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@ from flask_restful import Resource, Api
 
 from dotenv import dotenv_values
 from database.connect import create_db_and_tables
-from services.note import ProfileMeNotes
-from services.profile import ProfileMeView
+
+from services import ProfileMeView, ProfileMeNotes
 from utils.constants import HTTP_200_ACCEPTED
+from utils.celery import make_celery
+
 from services import (NoteListView, ProfileDetailsView, ProfileListView,
     ProfileSignupView, ProfileLoginView)
 
@@ -15,6 +17,12 @@ config = dotenv_values(".env")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = config.get('SECRET_KEY')
+app.config.update(
+    CELERY_RESULT_BACKEND='redis://localhost:6378',
+    CELERY_BROKER_URL='redis://localhost:6378'
+)
+
+celery = make_celery(app)
 api = Api(app)
 
 
@@ -23,6 +31,10 @@ def create_db():
     app.logger.info("Creating initial database")
     create_db_and_tables()
     app.logger.info("Initial database created")
+
+@celery.task()
+def add_together(a, b):
+    return a + b
 
 
 class HelloWorld(Resource):
@@ -35,7 +47,6 @@ api.add_resource(ProfileSignupView, '/signup')
 api.add_resource(ProfileLoginView, '/login')
 
 api.add_resource(ProfileMeView, '/me')
-
 
 api.add_resource(ProfileListView, '/profile')
 api.add_resource(ProfileDetailsView, '/profile/<int:profile_id>')
