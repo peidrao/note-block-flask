1970a9df3b3d9ac94de6144ab18b9f9a55edb80f
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-14 16:04:34 -0300

    refactor config

--- src/server/config.py
+++ src/server/config.py
@@ -1,30 +1,36 @@
+import os
 from dotenv import dotenv_values
+from pydantic import PostgresDsn
 
 config = dotenv_values(".env")
-
 
 class Config:
     SECRET_KEY = config.get('SECRET_KEY')
     DEBUG = True
-
-    DATABASE_USER = config.get('DATABASE_USER')
-    DATABASE_DB = config.get('DATABASE_DATA')
-    DATABASE_PASSWORD = config.get('DATABASE_PASS')
-    DATABASE_PORT = config.get('DATABASE_PORT')
-
-    HOST = config.get('HOST')
-
-    CELERY_BROKER_URL = config.get('CELERY_BROKER')
-    CELERY_RESULT_BACKEND = config.get('CELERY_RESULT')
-
 
 
 class TestConfig(Config):
     """Testing configuration options."""
     ENV_PREFIX = 'APP_'
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///memory'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///memory.db'
 
 
-config = Config()
-test_config = TestConfig()
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI: str = PostgresDsn.build(
+        scheme='postgresql+psycopg2',
+        path='/flask_db',
+        user=config.get('DATABASE_USER'),
+        password=config.get('DATABASE_PASS'),
+        host=config.get('HOST'),
+        port=config.get('DATABASE_PORT')
+    )
+    HOST = config.get('HOST')
+
+    CELERY_BROKER_URL = config.get('CELERY_BROKER')
+    CELERY_RESULT_BACKEND = config.get('CELERY_RESULT')
+
+
+available_configs = dict(development=DevelopmentConfig, test=TestConfig)
+selected_config = os.environ.get("FLASK_DEBUG")
+config = available_configs.get(selected_config)
