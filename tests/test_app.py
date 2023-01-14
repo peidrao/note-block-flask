c26d1fb0359e8c16a7606899801533c184e7641b
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-14 12:02:19 -0300

    add tests

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+import json
+
+
+def test_index(test_app):
+    client = test_app.test_client()
+    response = client.get('/')
+    assert json.loads(response.data) == {'hello': 'world'}
+    assert response.status_code == 200
