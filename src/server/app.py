f4dd8c96988f32e53545142df6e42f4a2503e3fe
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-17 22:53:00 -0300

    add cors

--- src/server/app.py
+++ src/server/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from src.server.config import config
 from src.server.routers import register_endpoints
+from flask_cors import CORS
+
 
 def create_app():
-
     app = Flask(__name__)
+    CORS(app)
+    # CORS(app, resources={r"/" {"origins": "http://localhost:8080"}})
+    app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_object(config)
-
     register_endpoints(app)
     return app
