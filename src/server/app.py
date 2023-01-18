8d5e1152a9d5e7d4d70fb0737937dbf072596d51
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-17 23:38:06 -0300

    add jwtmanager

--- src/server/app.py
+++ src/server/app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from src.server.config import config
 from src.server.routers import register_endpoints
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-    # CORS(app, resources={r"/" {"origins": "http://localhost:8080"}})
+    JWTManager(app)
+    # jwt.init_app(app)
     app.config['CORS_HEADERS'] = 'Content-Type'
     app.config.from_object(config)
     register_endpoints(app)
