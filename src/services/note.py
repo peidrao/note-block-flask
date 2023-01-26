30682b6f2c79593580ea6d32e0855f59be83197d
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-25 23:54:29 -0300

    create notes

--- src/services/note.py
+++ src/services/note.py
@@ -8,7 +8,6 @@ from src.database.connect import engine
 from src.models import Note, Profile
 from src.schemas.note import NoteSchema
 
-# from src.utils.auth import token_required
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from src.utils.constants import (
@@ -21,8 +20,9 @@ from src.utils.constants import (
 
 
 class NoteListView(MethodView):
-    @jwt_required
-    def post(self, _):
+    @jwt_required()
+    def post(self):
+        profile_id = get_jwt_identity()
         json_data = request.get_json()
         if not json_data:
             return {"message": "No payload"}, HTTP_400_BAD_REQUEST
@@ -30,13 +30,13 @@ class NoteListView(MethodView):
         try:
             data = NoteSchema().load(json_data)
         except ValidationError as err:
-            return err.messages, HTTP_422_UNPROCESSABLE_ENTITY
+            return {'error': err.messages}, HTTP_422_UNPROCESSABLE_ENTITY
 
         text = data["text"]
 
         with Session(engine) as session:
 
-            profile = session.get(Profile, self.id)
+            profile = session.get(Profile, profile_id)
             if profile:
                 note = Note(text=text, profile_id=profile.id)
                 session.add(note)
