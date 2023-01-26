5ce7e7c63fb52f195e1c58931197a1fa64b48bc7
Pedro Fonseca <contatopedrorn@gmail.com>
2023-01-25 23:52:30 -0300

    update database

--- src/database/connect.py
+++ src/database/connect.py
@@ -1,13 +1,5 @@
-from sqlmodel import create_engine, SQLModel
+from sqlmodel import create_engine
 from src.server.config import config
 
 
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI, echo=True)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-def drop_db_and_tables():
-    SQLModel.metadata.drop_all(engine)
